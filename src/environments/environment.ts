4201aea1f8d21a396b370e72eabcc0030fa375cc
mohammed-ashrf <greenbile2002@gmail.com>
2024-02-09 11:37:24 +0200

    licence

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
-    // apiUrl: 'http://192.168.1.24:8080',
+    // apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://192.168.1.24:8080',
     // apiUrl: 'http://23.97.188.100:8080',
     enableDebug: true,
 };
